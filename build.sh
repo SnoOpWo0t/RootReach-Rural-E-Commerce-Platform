668d0923157dc6b3e1d4a407b35439c079450903
SnoOpWo0t <afrinsultanaakhi138@gmail.com>
2026-06-30 19:03:03 +0600

    Remove unused build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Build successful! Skipping migrations and collectstatic since they are handled locally."
-
