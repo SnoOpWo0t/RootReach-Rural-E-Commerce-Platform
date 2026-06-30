580ccec2c803c6c863cd8078a953628590c6f396
SnoOpWo0t <afrinsultanaakhi138@gmail.com>
2026-06-30 19:18:04 +0600

    h

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+# Add the project root directory to the sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rootreach.settings')
 
--- rootreach/wsgi.py
+++ rootreach/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project root directory to the sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
